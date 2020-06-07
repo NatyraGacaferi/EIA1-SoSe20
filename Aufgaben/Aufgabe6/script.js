346c873bee35317fba65ea3c85dca92ddd243603
NatyraGacaferi <64071490+NatyraGacaferi@users.noreply.github.com>
2020-06-07 09:27:23 +0200

    aufgabe 6

---
+++ Aufgaben/Aufgabe6/script.js
@@ -0,0 +1,56 @@
+"use strict";
+function EuropeFunction() {
+    document.getElementById(".titleRegion").innerHTML = "Europe";
+    document.getElementById("headline1").innerHTML = "4209.3";
+    document.getElementById("text1").innerHTML = "Emission absolute of Europe in 2018";
+    document.getElementById("headline2").innerHTML = "13.528%";
+    document.getElementById("headline3").innerHTML = "-15.23%";
+    document.getElementById("headline4").innerHTML = "-756.4";
+    document.getElementById("chart").setAttribute("style", "height: 14.80%");
+}
+function NoAmFunction() {
+    document.getElementById("titleRegion").innerHTML = "North America";
+    document.getElementById("headline1").innerHTML = "6035.6";
+    document.getElementById("text1").innerHTML = "Emission absolute of North America in 2018";
+    document.getElementById("headline2").innerHTML = "19,397%";
+    document.getElementById("headline3").innerHTML = "-8.56%";
+    document.getElementById("headline4").innerHTML = "-564.8";
+    document.getElementById("chart").setAttribute("style", "height: 20.465%");
+}
+function SoAmFunction() {
+    document.getElementById("titleRegion").innerHTML = "South America";
+    document.getElementById("headline1").innerHTML = "1261.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of South America in 2018";
+    document.getElementById("headline2").innerHTML = "4.054%";
+    document.getElementById("headline3").innerHTML = "11.38%";
+    document.getElementById("headline4").innerHTML = "128.9";
+    document.getElementById("chart").setAttribute("style", "height: 4.126%");
+}
+function AfricaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Africa";
+    document.getElementById("headline1").innerHTML = "1235.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of Africa in 2018";
+    document.getElementById("headline2").innerHTML = "3,971%";
+    document.getElementById("headline3").innerHTML = "20,18%";
+    document.getElementById("headline4").innerHTML = "207.5";
+    document.getElementById("chart").setAttribute("style", "height: 3.754%");
+}
+function AsiaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Asia";
+    document.getElementById("headline1").innerHTML = "16274.1";
+    document.getElementById("text1").innerHTML = "Emission absolute of Asia in 2018";
+    document.getElementById("headline2").innerHTML = "52.301%";
+    document.getElementById("headline3").innerHTML = "25.62%";
+    document.getElementById("headline4").innerHTML = "3319.4";
+    document.getElementById("chart").setAttribute("style", "height: 50.197%");
+}
+function AustraliaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Australia";
+    document.getElementById("headline1").innerHTML = "2100.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of Australia in 2018";
+    document.getElementById("headline2").innerHTML = " 6.75%";
+    document.getElementById("headline3").innerHTML = "5.39%";
+    document.getElementById("headline4").innerHTML = "107.5";
+    document.getElementById("chart").setAttribute("style", "height: 6.654%");
+}
+//# sourceMappingURL=script.js.map
